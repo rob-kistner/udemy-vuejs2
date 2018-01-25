309341022b42983d16aaac8f1955405bffd4f4e3
rob-kistner <robkistner@mac.com>
2018-01-25 01:20:09 -0600

    Finished folder struct

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import Home from './Home.vue'
-
-import './assets/scss/styles.scss'
-
-
-Vue.component('app-servers', Home);
-
 new Vue({
-	el: '#app',
-	render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 })
