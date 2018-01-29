7770bb7c899a89d561a7a98806cd33556f87ed2e
rob-kistner <robkistner@mac.com>
2018-01-29 00:52:55 -0600

    Finished forms 1/29/18 PM

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,134 @@
 <template>
     <div class="container">
-        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
-        <div class="row">
-            <div class="col-12 text-center mt-4">
-                <div class="alert alert-info">
-                    Info: Click on a quote to delete it.
+        <form>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-8">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userData.password">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control"
+                                v-model="userData.age">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-8 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"
+                            v-model="message"></textarea>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-8">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-8 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"
+                                v-model="gender"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"
+                                v-model="gender"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-8 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control"
+                            v-model="selectedPriority"
+                            :selected="priority == 'Medium'">
+                        <option v-for="priority in priorities">{{priority}}</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-sm-10 col-md-8 from-group">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+                <div class="col-sm-10 col-md-8">
+                    <button
+                        class="btn btn-primary"
+                        @click.prevent="submitted">Submit!</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row justify-content-center" v-if="isSubmitted">
+            <div class="col-sm-10 col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h4>Your Data</h4>
+                        </div>
+                        <div class="card-text">
+                            <p>Email: <strong>{{userData.email}}</strong></p>
+                            <p>Password: <strong>{{userData.password}}</strong></p>
+                            <p>Age: <strong>{{userData.age}}</strong></p>
+                            <p style="white-space: pre">Message:<br><strong>{{message}}</strong></p>
+                            <p>Send Mail?</p>
+                            <ul v-if="sendMail.length > 0">
+                                <li v-for="item in sendMail"><strong>{{item}}</strong></li>
+                            </ul>
+                            <p>Gender: <strong>{{gender}}</strong></p>
+                            <p>Priority: <strong>{{selectedPriority}}</strong></p>
+                            <p>Switched: <strong>{{ dataSwitch }}</strong></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -14,40 +136,38 @@
 </template>
 
 <script>
-    import Header from './components/Header.vue';
-    import QuoteGrid from './components/QuoteGrid.vue';
-    import NewQuote from './components/NewQuote.vue';
+    import Switch from './components/Switch.vue';
 
     export default {
-        components: {
-            appHeader: Header,
-            appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote
-        },
-        data: function() {
+        data() {
             return {
-                quotes: [
-                    "Just a quote to see something",
-                    "A bird in the hand is worth two in the bush",
-                    "Don't count your chickens before they hatch",
-                    ],
-                maxQuotes: 10
+                userData: {
+                    email: '',
+                    password: '',
+                    age: 25,
+                },
+                message: 'Your message here...',
+                sendMail: [],
+                gender: '',
+                priorities: [
+                    'High', 'Medium', 'Low'
+                ],
+                selectedPriority: 'High',
+                dataSwitch: false,
+                isSubmitted: false
             }
         },
+        components: {
+            appSwitch: Switch
+        },
         methods: {
-            newQuote(quote) {
-                if(this.quotes.length >= this.maxQuotes) {
-                    return alert('Please delete quotes first');
-                }
-                this.quotes.push(quote);
-            },
-            deleteQuote(index) {
-                // remove 1 element at index
-                this.quotes.splice(index, 1);
+            submitted() {
+                this.isSubmitted = true;
             }
         }
     }
 </script>
 
 <style>
+
 </style>
---
+++ src/components/CourseHeader.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+	<header class="container-fluid">
+		<div class="container">
+			<p class="title">Udemy: {{courseName}}</p>
+			<p class="section">Section {{sectionNum}}: {{sectionName}}</p>
+		</div>
+	</header>
+
+	</div>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				courseName: 'VueJS 2 - The Complete Guide',
+				sectionNum: 11,
+				sectionName: 'Handling User Input with Forms'
+			}
+		}
+	};
+</script>
+
+<style scoped>
+	.container-fluid {
+		background-color: #bbb;
+		margin-bottom: 2rem;
+	}
+	.container {
+		padding: 1rem;
+		color: #fff;
+	}
+	.container p {
+		text-align: center;
+		margin-bottom: 0;
+		font-weight: 300;
+	}
+	.title {
+		font-size: 1.25rem;
+		}
+	.section {
+		font-size: 1.75rem;
+	}
+</style>
