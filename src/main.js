9294d91b1fec7c73a1b7c9029417a61d060e7c4d
rob-kistner <>
2018-02-03 09:56:25 -0600

    finished 15-214

--- src/main.js
+++ src/main.js
@@ -7,11 +7,25 @@ import VueResource from 'vue-resource';
 ------------------------------------------*/
 Vue.use(VueResource);
 
-/*
-	NOTE: $http, the $ is not needed at this level
-	this is the one url we'll use as a base for all database operations	
-------------------------------------------*/
 Vue.http.options.root = "https://vuejs-http-582b9.firebaseio.com/data.json";
+
+/* -----------------------------------------
+	Intercepts request and can manipulate it
+	from there
+
+	in this case, it transforms a POST request 
+	into a PUT request which overwrites everything
+	(yikes) and writes the two fields directly to 
+	the root without an index
+------------------------------------------*/
+Vue.http.interceptors.push((request, next) => {
+	console.log(request);
+	if(request.method == 'POST') {
+		request.method = 'PUT';
+	}
+	next();
+});
+
 
 new Vue({
 	el: '#app',
