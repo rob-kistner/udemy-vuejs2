d886f369e5267aa10b72dcfe118beb6ce752c0d9
rob-kistner <>
2018-02-06 09:54:29 -0600

    Got 17-257 up to the point where the babel install crashes it

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,10 +1,36 @@
 <template>
     <div>
-        <p>Counter is: {{ this.$store.getters.doubleCounter }}</p>
-        <p>Number of clicks {{ this.$store.getters.stringCounter }}</p>
+        <p>Counter is: {{ dCounter }}</p>
+        <p>Number of clicks {{ sCounter }}</p>
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+    export default {
+        /* -----------------------------------------
+            pull specified getters and reference in 
+            the component.
 
+            note how mapGetters hogs the whole computed
+            object as a function. There's a solution using 
+            ...mapGetters([]), i.e.: using the spread operator
+            to pull the getters individually, but it involves 
+            adding a babel plugin that seems to crash the 
+            npm install. Committing as 17-257-getters-to-props
+            before doing this install again
+        ------------------------------------------*/
+        // computed: mapGetters([
+        //     'doubleCounter',
+        //     'stringCounter'
+        // ])
+        /*----------------------------------------
+            can also map to different names by
+            passing an object like this
+        ------------------------------------------*/
+        computed: mapGetters({
+            dCounter: 'doubleCounter',
+            sCounter: 'stringCounter'
+        })
+    }
 </script>
