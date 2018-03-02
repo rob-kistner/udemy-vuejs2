0e4fb9a10b0ed6483a9b3a68ef168659b4d8d830
rob-kistner <dev@robkistner.com>
2018-03-02 14:19:03 -0600

    finished 18-287, again

---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+    .col-sm-6.col-md-4.my-4
+        .card.card-success
+            .card-header.bg-info.text-white
+                h4 {{ stock.name }}
+            .card-body
+                .card-text
+                    small Price: {{ stock.price }} Quantity: {{ stock.quantity }}
+                    input.form-control(type="number", placeholder="Quantity", v-model.number="quantity", :class="{danger: insufficientQuantity}")
+                    button.btn.btn-info.mt-3(@click="sellStock", :disabled="insufficientQuantity || quantity<=0 || !Number.isInteger(quantity)")
+                        | {{ insufficientQuantity ? "Not enough stocks" : "Sell" }}
+</template>
+
+<script>
+    import {mapActions} from 'vuex';
+
+    export default {
+        props: [
+            'stock'
+        ],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        computed: {
+            insufficientQuantity() {
+                return this.quantity > this.stock.quantity;
+            }
+        },
+        methods: {
+            ...mapActions({
+                placeSellOrder: 'sellStock'
+            }),
+            sellStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(this.quantity + ', '+ this.stock.quantity);
+                this.placeSellOrder(order);
+                this.quantity = 0;
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .danger {
+        border: 2px solid #f00;
+    }
+</style>
